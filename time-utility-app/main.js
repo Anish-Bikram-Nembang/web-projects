b2de4663ab9c702b827a4d54b26c98b5f6ba7a90
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-02-23 18:49:17 +0000

    fin(timer): finish timer

--- time-utility-app/main.js
+++ time-utility-app/main.js
@@ -1,4 +1,6 @@
 "use strict";
+
+//Date and Time
 class DateTime {
   constructor(timeElement, dateElement) {
     this.time = document.getElementsByClassName(timeElement)[0];
@@ -7,8 +9,9 @@ class DateTime {
 }
 const dateAndTime = new DateTime("time", "date");
 
+//StopWatch
 class StopWatch {
-  #running = false;
+  running = false;
   constructor(className) {
     this.time = 0;
     this.startTime = 0;
@@ -16,20 +19,14 @@ class StopWatch {
     this.element = document.getElementsByClassName(className)[0];
   }
   start() {
-    if (this.#running === false) {
-      this.#running = true;
+    if (this.running === false) {
+      this.running = true;
       this.startTime = Date.now();
-      this.startIntervalID = setInterval(() => {
-        const currentTime = Date.now();
-        const total = this.elapsedTime + (currentTime - this.startTime);
-        this.time = (total / 1000).toFixed(2);
-      }, 16);
     }
   }
   stop() {
-    if (this.#running) {
-      clearInterval(this.startIntervalID);
-      this.#running = false;
+    if (this.running) {
+      this.running = false;
       this.elapsedTime += Date.now() - this.startTime;
     }
   }
@@ -37,14 +34,9 @@ class StopWatch {
     this.stop();
     this.startTime = 0;
     this.elapsedTime = 0;
-    this.time = "0.00";
+    this.time = 0;
   }
 }
-class Timer {
-  //Todo
-}
-
-//for stopwatch
 const watch = new StopWatch("stop-watch-time");
 const stopWatchStartButton = document.getElementById("stopwatch-start");
 const stopWatchStopButton = document.getElementById("stopwatch-stop");
@@ -54,26 +46,84 @@ stopWatchStartButton.addEventListener("click", watch.start.bind(watch));
 stopWatchStopButton.addEventListener("click", watch.stop.bind(watch));
 stopWatchResetButton.addEventListener("click", watch.reset.bind(watch));
 
-//for timer
+//Timer
+class Timer {
+  running = false;
+  constructor(className) {
+    this.endTime = 0;
+    this.remaining = 0;
+    this.time = 0;
+    this.element = document.getElementsByClassName(className)[0];
+  }
+  increment() {
+    this.time += 1000;
+    this.remaining = this.time;
+  }
+  decrement() {
+    if (this.time >= 1000) {
+      this.time -= 1000;
+      this.remaining = this.time;
+    }
+  }
+  start() {
+    if (this.running === false && this.remaining > 0) {
+      this.running = true;
+      this.endTime = Date.now() + this.remaining;
+    }
+  }
+  stop() {
+    if (this.running) {
+      this.running = false;
+      this.remaining = this.endTime - Date.now();
+    }
+  }
+  reset() {
+    this.running = false;
+    this.remaining = 0;
+    this.time = 0;
+  }
+}
 const timer = new Timer("timer-time");
 const timerIncrementButton = document.getElementById("timer-increment");
 const timerDecrementButton = document.getElementById("timer-decrement");
 const timerStartButton = document.getElementById("timer-start");
+const timerStopButton = document.getElementById("timer-stop");
 const timerResetButton = document.getElementById("timer-reset");
 
-// timerIncrementButton.addEventListener("click");
-// timerDecrementButton.addEventListener("click");
-// timerStartButton.addEventListener("click");
-// timerResetButton.addEventListener("click");
+timerIncrementButton.addEventListener("click", timer.increment.bind(timer));
+timerDecrementButton.addEventListener("click", timer.decrement.bind(timer));
+timerStartButton.addEventListener("click", timer.start.bind(timer));
+timerStopButton.addEventListener("click", timer.stop.bind(timer));
+timerResetButton.addEventListener("click", timer.reset.bind(timer));
 
 //main data to view function
 function dataToView() {
-  watch.element.textContent = watch.time;
-
   //to update date and time
   const dateAndTimeElement = new Date();
   dateAndTime.time.textContent = dateAndTimeElement.toLocaleTimeString();
   dateAndTime.date.textContent = dateAndTimeElement.toLocaleDateString();
-}
 
-setInterval(dataToView, 1000);
+  //to update stop watch
+  if (watch.running) {
+    const currentTime = Date.now();
+    const total = watch.elapsedTime + (currentTime - watch.startTime);
+    watch.element.textContent = (total / 1000).toFixed(2);
+  } else if (watch.elapsedTime === 0) {
+    watch.element.textContent = Number(0).toFixed(2);
+  }
+
+  //to update timer
+  if (timer.running) {
+    timer.remaining = timer.endTime - Date.now();
+
+    if (timer.remaining <= 0) {
+      timer.remaining = 0;
+      timer.running = false;
+    }
+
+    timer.element.textContent = (timer.remaining / 1000).toFixed(2);
+  } else {
+    timer.element.textContent = (timer.remaining / 1000).toFixed(2);
+  }
+}
+setInterval(dataToView, 100);
